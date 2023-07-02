5de2d1496af3f756180b45e66540a33f75b5a1d3
quaspar33 <kacper.ziebacz01@gmail.com>
2023-07-02 12:38:25 +0200

    refactor directory of components

--- eventorganiser_react/src/App.js
+++ eventorganiser_react/src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import MenuBar from './MenuBar.js';
-import LoginWindow from "./LoginWindow";
+import MenuBar from './componets/MenuBar.js';
+import LoginWindow from "./componets/LoginWindow";
 
 function App() {
     return (
