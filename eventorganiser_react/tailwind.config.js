43379b2c27c3f81fac991141f78781a24e4116d1
quaspar33 <kacper.ziebacz01@gmail.com>
2023-07-09 13:22:59 +0200

    modifying tailwind.config.js to add custom colors, fonts, and assets

--- eventorganiser_react/tailwind.config.js
+++ eventorganiser_react/tailwind.config.js
@@ -1,11 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: "jit",
+  darkMode: true,
+  purge: [
+      "./src/**/*.{js,ts,jsx,tsx}",
+      "./index.html"
+  ],
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-1": "#F0F5FF",
+        "color-2": "#E5EDFF",
+        "color-3": "#CDDBFE",
+        "color-4": "#B4C6FC",
+        "color-5": "#8DA2FB",
+        "color-6": "#6875F5",
+        "color-7": "#5850EC",
+        "color-8": "#5145CD",
+        "color-9": "#42389D",
+        "color-10": "#362F78"
+      },
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      content: {
+        waves: "url('./assets/Wave.svg')",
+        circles: "url('./assets/IrregularCircle.svg)",
+        lecture1: "url('./assets/Lecture_1.svg')",
+        lecture2: "url('./assets/Lecture_2.svg')",
+        lecture3: "url('./assets/Lecture_3.svg')",
+        lecture4: "url('./assets/Lecture_4.svg')",
+        lecture5: "url('./assets/Lecture_5.svg')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
   },
   plugins: [],
 }
