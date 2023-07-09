97e3339061ff2a59fdf625584345e01b159aa57e
quaspar33 <kacper.ziebacz01@gmail.com>
2023-07-09 16:08:19 +0200

    addition of Menu symbol

--- eventorganiser_react/src/components/MenuBar.tsx
+++ eventorganiser_react/src/components/MenuBar.tsx
@@ -7,12 +7,12 @@ const MenuBar: React.FC = () => {
     };
 
     return (
-        <div className="flex items-center h-12 px-4 bg-color-3">
+        <div className = "flex items-center h-12 px-4 bg-color-3">
             <button
-                className="p-1 rounded-md hover:bg-gray-200 focus:outline-none"
-                onClick={handleClick}
+                className = "p-1 rounded-md hover:bg-gray-200 focus:outline-none"
+                onClick = { handleClick }
             >
-                <Bars3Icon className="h-6 w-6 text-color-10" />
+                <Bars3Icon className = "h-6 w-6 text-color-10"/>
             </button>
         </div>
     );
