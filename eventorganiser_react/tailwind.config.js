3dbc557e6cf589bc286045157c485257c95836aa
quaspar33 <kacper.ziebacz01@gmail.com>
2023-07-10 12:28:28 +0200

    addition of dropdown menu

--- eventorganiser_react/tailwind.config.js
+++ eventorganiser_react/tailwind.config.js
@@ -9,16 +9,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "color-1": "#F0F5FF",
-        "color-2": "#E5EDFF",
-        "color-3": "#CDDBFE",
-        "color-4": "#B4C6FC",
-        "color-5": "#8DA2FB",
-        "color-6": "#6875F5",
-        "color-7": "#5850EC",
-        "color-8": "#5145CD",
-        "color-9": "#42389D",
-        "color-10": "#362F78"
+        "light-blue": "#CDDBFE",
+        "app-background": "#5145CD",
+        "deep-blue": "#362F78"
       },
       fontFamily: {
         dmsans: ["DM Sans", "sans-serif"],
