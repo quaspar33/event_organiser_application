e7c42cf1da2e1bb9f1bda76ac4d3a209e6765730
quaspar33 <kacper.ziebacz01@gmail.com>
2023-07-15 01:31:14 +0200

    add text fields and working icons on images

--- eventorganiser_react/src/components/BackgroundSlider.tsx
+++ eventorganiser_react/src/components/BackgroundSlider.tsx
@@ -1,6 +1,7 @@
 import ImageSlider from "./Images.tsx";
 import { useState, useEffect } from "react";
 import MenuBar from "./MenuBar.tsx";
+import { StopIcon } from "@heroicons/react/24/outline";
 
 function BackgroundSlider() {
     const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
@@ -8,10 +9,14 @@ function BackgroundSlider() {
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentSlideIndex((prevSlideIndex) => (prevSlideIndex + 1) % ImageSlider.length);
-        }, 3000);
+        }, 4000);
 
         return () => clearInterval(interval);
     }, []);
+
+    const goToSlide = ({ index }: { index: any }) => {
+        setCurrentSlideIndex(index);
+    };
 
     return (
         <div className="relative h-full w-full">
@@ -21,13 +26,33 @@ function BackgroundSlider() {
                     className={`absolute top-0 left-0 w-full h-full bg-center bg-cover transition-opacity duration-1000 ${
                         index === currentSlideIndex ? "opacity-100" : "opacity-0"
                     }`}
-                    style={{ backgroundImage: `url(${slide.url})` }}
-                ></div>
+                    style={{
+                        backgroundImage: `url(${slide.url})`
+                    }}
+                >
+                <div
+                        className={`h-full flex flex-col justify-center items-left ml-72 text-white ${
+                            index === currentSlideIndex ? "opacity-100" : "opacity-0"
+                        }`}
+                    >
+                    <h1 className="text-6xl text-color-10 font-bold">{slide.title}</h1>
+                    <p className="text-3xl text-color-10 font-bold">{slide.body}</p>
+                    <div className="flex mt-4">
+                        {ImageSlider.map((_, i) => (
+                            <StopIcon
+                                key={i}
+                                className={`text-color-10 h-4 w-4 cursor-pointer mr-2 font-bold ${
+                                    i === currentSlideIndex ? "opacity-100" : "opacity-50"
+                                }`}
+                                onClick={() => goToSlide({ index: i })}
+                            />
+                        ))}
+                    </div>
+                    </div>
+                </div>
             ))}
             <div className="h-full">
-                {
-                    <MenuBar/>
-                }
+                <MenuBar />
             </div>
         </div>
     );
--- eventorganiser_react/src/components/Images.tsx
+++ eventorganiser_react/src/components/Images.tsx
@@ -1,7 +1,6 @@
 import Lecture1 from "../assets/Lecture_1.svg";
 import Lecture2 from "../assets/Lecture_2.svg";
 import Lecture3 from "../assets/Lecture_3.svg";
-import Lecture4 from "../assets/Lecture_4.svg";
 
 const ImageSlider = [
     {
@@ -17,11 +16,6 @@ const ImageSlider = [
     {
         url: Lecture3,
         title: "Picture 3",
-        body: "coś tam coś tam"
-    },
-    {
-        url: Lecture4,
-        title: "Picture 4",
         body: "coś tam coś tam"
     }
 ];
